7e7e35e71adbdd7ac88e038d24da417d4c852453
Cory LaFlamme <me@corylaflamme.com>
2014-04-01 17:33:05 -0400

    Removed getter functions and replaced them with inline lambda functions.

--- Menu.lua
+++ Menu.lua
@@ -1,45 +1,6 @@
 sc.menuId = sc.menu:CreateControlPanel('scConfig', 'SimpleClock')
 
 -- -----------------------------------------------------------------------------
--- 24 hour time format
--- -----------------------------------------------------------------------------
-function getUse24Hour()
-	return sc.sv.use24Hour
-end
-
--- -----------------------------------------------------------------------------
--- Lowercase meridiem indicator
--- -----------------------------------------------------------------------------
-function getLowercaseMeridiem()
-	return sc.sv.lowercaseMeridiem
-end
-
--- -----------------------------------------------------------------------------
--- Visible meridiem indicator
--- -----------------------------------------------------------------------------
-function getHideMeridiem()
-	return sc.sv.hideMeridiem
-end
-
--- -----------------------------------------------------------------------------
--- Meridiem dots
--- -----------------------------------------------------------------------------
-function getNoDotsMeridiem()
-	return sc.sv.noDotsMeridiem
-end
-
--- -----------------------------------------------------------------------------
--- Hide with overlay
--- -----------------------------------------------------------------------------
-function getHideWithOverlay()
-	return sc.sv.hideWithOverlay
-end
-
-function getTextAlign()
-	return sc.sv.textAlign
-end
-
--- -----------------------------------------------------------------------------
 -- Visibility
 -- -----------------------------------------------------------------------------
 sc.menu:AddHeader(sc.menuId, 'scHeaderVisibility', 'Visibility')
@@ -50,7 +11,7 @@ sc.menu:AddCheckbox(
 	'scConfigChkbox_HideWithOverlay',
 	'Hide when viewing menus',
 	'Hides the clock when viewing inventory or similar screens.',
-	getHideWithOverlay,
+	function() return sc.sv.hideWithOverlay end,
 	sc.ui.toggleHideWithOverlay
 )
 
@@ -65,7 +26,7 @@ sc.menu:AddCheckbox(
 	'scConfigChkbox_24HourTime',
 	'24 Hour (Military) Time',
 	'Choose whether to use 24hr time (00:30) or 12hr time (12:30AM).',
-	getUse24Hour,
+	function() return sc.sv.use24Hour end,
 	sc.ui.toggleUse24Hour
 )
 
@@ -75,7 +36,7 @@ sc.menu:AddCheckbox(
 	'scConfigChkbox_VisibleMeridiem',
 	'Hide Meridiem Indicator',
 	'Disable the A.M./P.M. text.',
-	getHideMeridiem,
+	function() return sc.sv.hideMeridiem end,
 	sc.ui.toggleHideMeridiem
 )
 
@@ -85,7 +46,7 @@ sc.menu:AddCheckbox(
 	'scConfigChkbox_LowercaseMeridiem',
 	'Lowercase Meridiem Indicator',
 	'Have the A.M./P.M. text display in lowercase.',
-	getLowercaseMeridiem,
+	function() return sc.sv.lowercaseMeridiem end,
 	sc.ui.toggleLowercaseMeridiem
 )
 
@@ -95,7 +56,7 @@ sc.menu:AddCheckbox(
 	'scConfigChkbox_NoDotsMeridiem',
 	'Hide Dots in Meridiem Indicator',
 	'Hide the dots in A.M./P.M.',
-	getNoDotsMeridiem,
+	function() return sc.sv.noDotsMeridiem end,
 	sc.ui.toggleNoDotsMeridiem
 )
 
@@ -104,13 +65,14 @@ sc.menu:AddCheckbox(
 -- -----------------------------------------------------------------------------
 sc.menu:AddHeader(sc.menuId, 'scHeaderPositioning', 'Positioning')
 
+-- Text alignment
 sc.menu:AddDropdown(
 	sc.menuId,
 	'scConfigDrpdwn_TextAlign',
 	'Text Alignment',
 	'Which way the numbers on the clock should expand from.',
 	{'Left', 'Right', 'Center'},
-	getTextAlign,
+	function() return sc.sv.textAlign end,
 	function(val) sc.ui:setTextAlign(val) end
 
 )
