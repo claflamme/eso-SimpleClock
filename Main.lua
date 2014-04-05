c28baae16555857f13426a678143c9a55ff89b08
Cory LaFlamme <me@corylaflamme.com>
2014-04-05 15:42:15 -0400

    Added docblock for bufferReached().

--- Main.lua
+++ Main.lua
@@ -27,20 +27,25 @@ sc.config = {
 	svName = 'SimpleClock_SavedVariables'
 }
 
---- Control panel menu
+-- Load libraries
 LAM = LibStub:GetLibrary('LibAddonMenu-1.0')
 LMP = LibStub:GetLibrary("LibMediaProvider-1.0")
 
-
-
---- Basic event buffering, cribbed from http://wiki.esoui.com/Event_%26_Update_Buffering
-function sc:bufferReached(key, buffer)
+---
+-- Basic event buffering, from http://wiki.esoui.com/Event_%26_Update_Buffering
+--
+-- @param  key      Unique string for the buffer name to start or check.
+-- @param  seconds  Number of seconds to hold up the buffer for.
+--
+-- @return  bufferReady  Boolean indicating whether or not it's ok to proceed.
+-- -----------------------------------------------------------------------------
+function sc:bufferReached(key, seconds)
 
 	if sc.buffer[key] == nil then
 		sc.buffer[key] = {}
 	end
 
-	sc.buffer[key].buffer = buffer or 1
+	sc.buffer[key].buffer = seconds or 1
 	sc.buffer[key].now = GetFrameTimeSeconds()
 
 	if sc.buffer[key].last == nil then
